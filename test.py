136b71ca37cdb78f3a2016c8f8ec02bc78607040
FightingLynx <32548@ma-web.nl>
2021-09-14 17:04:38 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
-print('Hello World!')
-
 getal = [4+2, 6-5, 7/3, 7*3, 4%3, 19//3]
 
 for i in range(0, len(getal)):
     print(getal[i])
+
+# logs:
+#   6
+#   1
+#   2.3333333333333335
+#   21
+#   1
+#   6
